346c51524a89fd299b87827d2fd6eb57a2487c13
LeezeeGD <elizabethuwakwe@gmail.com>
2021-09-28 17:01:31 +0100

    added slick.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,18 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      // backgroundImage: {
+      //   'hero-bg': "url('./build/public/first-dsk-png')",
+      // }
+      colors: {
+        mg: {
+          primary: "#082447",
+          secondary: "#DF4041",
+          light: "#E0E0E0"
+        }
+    },
+    },
   },
   variants: {
     extend: {},
